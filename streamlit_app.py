3bcfbc388bf4ad3830f3301084ffc60633272fa7
Nikhil Ambati <130098870+nambatibuf@users.noreply.github.com>
2024-04-04 01:16:18 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,7 +41,7 @@ if page == 'Home':
     st.markdown(
         """
         <div style="text-align:center;">
-            <img src="your_image_url.jpeg" alt="Nikhil Ambati" style="border-radius:50%; width:200px; height:200px; object-fit:cover; margin-bottom:10px;">
+            <img src="https://github.com/nambatibuf/streamlit/blob/8c83389532e8c9d4a35c3127cc1345e96e4addb0/your_image_url.jpeg" alt="Nikhil Ambati" style="border-radius:50%; width:200px; height:200px; object-fit:cover; margin-bottom:10px;">
         </div>
         """,
         unsafe_allow_html=True,
