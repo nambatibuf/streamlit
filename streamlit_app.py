dbdd6e5ff2adecb931c62e2ce488bd35c530d59f
Nikhil Ambati <130098870+nambatibuf@users.noreply.github.com>
2024-04-04 02:34:26 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -71,11 +71,13 @@ if page == 'Home':
     - 🚀 Aim to leverage my skills in data analysis and engineering to make a significant impact in the field of data science.
     """)
     
-    st.markdown("### Projects")
     st.markdown("""
     - **Sales Flow Pro**: Developed an analytics pipeline integrating Python, Airflow, GCP, and BigQuery, culminating in a Looker Studio dashboard for data quality checks.
     - **AI-Driven SQL Queries**: Transformed sales data into BCNF compliance using PySpark and PostgreSQL, and created a local website for AI-driven SQL query capabilities.
-    """)
+    - **Data-Driven Agricultural Insights**: Automated data pipeline utilizing Azure Data Factory, Databricks, and Delta Lake to process USDA datasets. This ensures real-time, actionable insights through Power BI visualizations and a predictive analytics dashboard hosted online.
+    - **Advanced Analytics and Machine Learning Integration**: Developed sophisticated machine learning models to predict crop yields, classifying them as high or low. Enabled monthly retraining to refine predictions, leveraging transformed data in Delta Lake.
+    - **User-Centric Solution Deployment**: Launched an interactive web platform allowing users to input parameters and receive yield predictions. This showcases a commitment to accessibility and user engagement in the agricultural sector.
+    """, unsafe_allow_html=True)
     
     st.markdown("### Technical Skills")
     st.markdown("""
